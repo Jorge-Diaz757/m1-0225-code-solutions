9f1cd0054496793e14132898dfeb71b17d68fc08
Jorge Diaz <j.diazm757@gmail.com>
2025-03-18 02:07:14 +0000

    Completed typescript-primitives-and-variables exercise

---
+++ typescript-primitives-and-variables/main.ts
@@ -0,0 +1,19 @@
+const fullName: string = 'Jorge Diaz Martinez';
+console.log('value of fullName: ', fullName);
+console.log('typeof fullName: ', typeof fullName);
+
+const isCool: boolean = true;
+console.log('value of isCool: ', isCool);
+console.log('typeof isCool: ', typeof isCool);
+
+const totalPets: number = 11;
+console.log('value of totalPets: ', totalPets);
+console.log('typeof totalPets: ', typeof totalPets);
+
+const empty: null = null;
+console.log('value of empty: ', empty);
+console.log('typeof empty: ', typeof empty);
+
+let nothing: undefined;
+console.log('value of nothing: ', nothing);
+console.log('typeof nothing: ', typeof nothing);
